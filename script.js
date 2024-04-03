5f4e2983713806d28106ec4950d77f05b1695b6e
ViniMSLima <vinisary.lima@gmail.com>
2024-04-03 09:07:06 -0300

    top

---
+++ script.js
@@ -0,0 +1,39 @@
+var nomeGlobal;
+var mensagemGlobal;
+var dateGlobal;
+
+function conferirMensagemWhatsApp() {
+    var nome = document.getElementById("nome").value;
+    var mensagem = document.getElementById("mensagem").value;
+    var date = new Date();
+
+    document.getElementById("conf").style.display = "inline"
+
+    nomeGlobal = nome;
+    mensagemGlobal = mensagem;
+    dateGlobal = date.toLocaleString(["pt-br"]);
+
+    document.getElementById("confNome").textContent = nome;
+    document.getElementById("confMsg").textContent = mensagem;
+    document.getElementById("confDate").textContent = dateGlobal;
+}
+
+function enviar() {
+    var numeroTelefone = "5541999999999";
+
+    var linkWhatsApp =
+        "https://wa.me/" +
+        numeroTelefone +
+        "?text=NOME DO RECEPTOR(A): " +
+        nomeGlobal +
+        " - MENSAGEM: " +
+        mensagemGlobal +
+        " - DATA: " +
+        dateGlobal;
+
+    window.open(linkWhatsApp, '_blank');
+}
+
+function format(data) {
+
+}
